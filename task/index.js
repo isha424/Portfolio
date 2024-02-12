e7605b2e6322d1dba33ce6879654103b3268e570
isha424 <tariqisha50@gmail.com>
2024-02-12 14:44:30 +0500

    Portfolio

---
+++ task/index.js
@@ -0,0 +1,124 @@
+ //let age=120;
+//let x=age;
+//console.log(x)
+
+//console.log(x>18)
+//if(x<18){
+//console.log("You Are A Minor " )
+//}
+//else if(x>=18 && x<=65){
+    //console.log("You Are Adult")
+
+//}
+//else if(x>65) {
+    //console.log("You Are A Senior Citizen")
+
+//}
+//Loops
+  //for (let index = 0; index < 10; index++) {
+    // if(index<=5)
+    
+    // console.log("value is less than 5")  
+    // //}
+
+
+//      multifunc(1,2)
+//      function multifunc(a,b){
+//          console.log(a*b/b*a)   
+//      }
+
+//     //object 
+//     const user={
+//         name: "Isha",
+//         roll: 12,
+//         City: "Lahore",
+        
+//     }
+
+// console.log(user.name,user.roll,user.City)
+    
+//     const Name={
+//         name:"isha",
+//         age: 19,
+//         gender: "Female",
+//         City: "lahore",
+
+//     };
+//     // Array
+//     const name2=["isha",19,"female","lahore"
+
+//     ]
+//     console.log(name2.length)
+
+// Sync and async 
+
+// function f1(){
+//       console.log("hello1")
+// }
+// function f2(){
+//     console.log("hello2")
+// }
+// function f3(){
+//     console.log("hello3")
+// }
+// function f4(){
+//     console.log("hello4")
+// }
+// function f5(){
+//     console.log("hello5")
+// }
+// function f6(){
+//     console.log("hello6")
+// }
+// function f7(){
+//     console.log("hello7")
+// }
+// function main(){
+//      setTimeout(()=>f1(),0)
+//      setTimeout(()=>f3(),3000)
+//      setTimeout(()=>f6(),2000)
+//      setTimeout(()=>f4(),1000)
+//      setTimeout(()=>f5(),4000)
+//      f2()
+//      f7()
+// }
+// main()
+
+// calculator
+function multiply(){
+    (x=5,y=9)
+    console.log(x*y)
+}
+function divide(){
+    (x=10,y=2)
+    console.log(x/y)
+}
+function addition(){
+    (x=2,y=2)
+    console.log(x+y)
+}
+function subtraction(){
+    (x=5,y=3)
+    console.log(x-y)
+}
+function multifunc(){
+    (x=5,y=3)
+    console.log(x*y3)
+}
+function main(){
+    setTimeout(()=>multiply(),0)
+    setTimeout(()=>divide(),3000)
+    setTimeout(()=>addition(),2000)
+    setTimeout(()=>subtraction(),1000)
+    setTimeout(()=>multifunc(),0)
+
+}
+main()
+
+
+
+    
+
+
+
+
